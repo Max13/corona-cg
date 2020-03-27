173b86c4a1a961dfefa66c25cbf92a1ef50ce6df
Adnan RIHAN <adnan@rihan.fr>
2020-03-27 20:32:53 +0100

    Added large footer with social links

--- source/_layouts/footer.blade.php
+++ source/_layouts/footer.blade.php
@@ -1,13 +1,52 @@
 <footer class="footer-1">
+    <div class="footer-area">
+        <div class="container">
+            <div class="row">
+               <div class="col-md-5 col-sm-5 col-xs-12">
+                    <div class="footer-content logo-footer">
+                        <div class="footer-head">
+                            <div class="footer-logo">
+                                <h4>À propos</h4>
+                            </div>
+                            <p class="text-justify">Ce projet a été réalisé par quelques membres de Congo-Tech, une communauté de congolais passionnés du numérique.</p>
+                        </div>
+                    </div>
+                </div>
+                <!-- end single footer -->
+                <div class="col-md-offset-2 col-md-5 col-sm-5 col-xs-12">
+                    <div class="footer-content last-content">
+                        <div class="footer-head">
+                            <h4>Rejoignez-nous</h4>
+
+                            <div class="footer-icons">
+                                <ul>
+                                    <li>
+                                        <a href="https://www.facebook.com/StopCovid19CG" target="_blank">
+                                            <i class="fa fa-facebook"></i>
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="https://twitter.com/StopCovid19CG" target="_blank">
+                                            <i class="fa fa-twitter"></i>
+                                        </a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
     <div class="footer-area-bottom">
         <div class="container">
             <div class="row">
                 <div class="col-xs-2">
-                    <a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/4.0/"><img alt="Licence Creative Commons" style="border-width:0" src="https://i.creativecommons.org/l/by-nc-sa/4.0/88x31.png" /></a>
+                    <a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/4.0/"><img alt="Licence Creative Commons" style="border-width:0" src="https://i.creativecommons.org/l/by-nc-sa/4.0/88x31.png"></a>
                 </div>
 
                 <div class="col-xs-3 col-xs-offset-10 col-sm-offset-2 copyright text-center">
-                    <a href="#">Congo-Tech</a> – 2020</p>
+                     2020 – <a href="#">Congo-Tech</a><p></p>
                 </div>
             </div>
         </div>
