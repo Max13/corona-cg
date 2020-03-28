0fc9bab81a8f10995efefc09b781276febd57285
Adnan RIHAN <adnan@rihan.fr>
2020-03-28 18:02:23 +0100

    Added 404 error page

---
+++ source/404.blade.php
@@ -0,0 +1,31 @@
+---
+permalink: 404.html
+---
+
+@extends('_layouts.master')
+
+@section('body')
+
+    <div class="page-head bg-color area-padding">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12 col-sm-12 col-xs-12">
+                    <div class="row">
+                        <div class="error-page">
+                            <!-- map area -->
+                            <div class="col-md-12 col-sm-12 col-xs-12">
+                                <div class="error-main-text text-center">
+                                    <h2 class="error-easy-text">Page introuvable</h2>
+                                    <h1 class="high-text">4<span class="color">0</span>4</h1>
+                                    <h3 class="error-bot">Oops, la page que vous recherchez n'existe pas.</h3>
+                                    <a  class="error-btn" href="/">Retour à l'accueil</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+@endsection
